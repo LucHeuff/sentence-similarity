f3619c4e8448be1c5fe097249965232db7dbb01f
Luc Heuff <lucheuff@hotmail.com>
2023-05-16 10:32:30 +0200

    Added documentation wherever necessary

--- src/vocab.py
+++ src/vocab.py
@@ -14,11 +14,20 @@ join_function = Callable[[list[str]], str]
 
 
 class Tokenizer:
+    """Used to tokenize and de-tokenize string sentences.
+    Not intended to be created directly, but generated through factory that fills in split and join functions.
+    """
     def __init__(self, 
                  lower: bool, 
                  split_function: split_function, 
                  join_function: join_function
                  ) -> None:
+        """
+        Args:
+            lower (bool): whether strings should be converted to lower case
+            split_function: function with which to split string sentences
+            join_function: function with which to recombine tokens into string sentences
+        """
         self.lower = lower
         self.split_function = split_function
         self.join_function = join_function
@@ -47,9 +56,23 @@ def join_characters(tokens: list[str]) -> str:
     return "".join(tokens)
 
 def word_tokenizer_factory(lower: bool) -> Tokenizer:
+    """Generates a Tokenizer that splits sentences into words on spaces, 
+    and can recombine them correctly.
+
+    Args:
+        lower (bool): whether all strings should be converted to lowercase.
+                      NOTE: this means that the Tokenizer cannot restore case sensitivity when decoding!
+    """
     return Tokenizer(lower, split_function=split_words, join_function=join_words)
 
 def character_tokenizer_factory(lower: bool) -> Tokenizer:
+    """Generates a Tokenizer that splits sentences into separate characters, 
+    and can recombine them correctly.
+
+    Args:
+        lower (bool): whether all strings should be converted to lowercase.
+                      NOTE: this means that the Tokenizer cannot restore case sensitivity when decoding!
+    """
     return Tokenizer(lower, split_function=split_characters, join_function=join_characters)
 
         
@@ -66,12 +89,19 @@ sentence_combiner = dict(
 )
 
 class Vocab:
+    """Maintains a vocabulary that can translate sentences to numbers and back."""
     def __init__(
             self, 
             sentences: list[str], 
             tokenize_method: str='words', 
             lower: bool=False
         ) -> None:
+        """
+        Args:
+            sentences (list[str]): all sentences from which the vocabulary should be generated.
+            tokenize_method (str, optional): whether tokenization should separate out 'words' or 'characters'. Defaults to 'words'.
+            lower (bool, optional): whether sentences need to be converted to lowercase prior to generating the vocabulary and encoding. Defaults to False.
+        """
         self.tokenizer, self.combine_sentences = self._create_tokenizer_and_combiner(tokenize_method, lower)
         self.vocab = self._create_vocab(sentences)
         self.decoder = self._create_decoder()
@@ -96,9 +126,27 @@ class Vocab:
         return {value: key for key, value in self.vocab.items()}       
 
     def encode(self, sentence: str) -> list[int]:
+        f"""Encodes a sentence through the vocabulary to a list of integers.
+        Any tokens that are not recognised in the vocabulary will be represented by {UNK}, or 0 after encoding
+
+        Args:
+            sentence (str): the sentence to be encoded
+
+        Returns:
+            list[int]: the encoded sentence as a list of integers
+        """
         return self.tokenizer.encode(sentence, self.vocab)
     
     def decode(self, numbered_sentence: list[int]) -> str:
+        f"""Decodes a list of integers through the vocabulary to a sentence.
+        Any tokens that were not recognised in the vocabulary, represented by 0, will be represented by {UNK} after decoding
+
+        Args:
+            numbered_sentence (list[int]): list of integers representing a stence
+
+        Returns:
+            str: the decoded sentence as a string
+        """
         return self.tokenizer.decode(numbered_sentence, self.decoder)
     
     def __len__(self) -> int:
