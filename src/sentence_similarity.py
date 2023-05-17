c0130e5364af40dc1b13579a111119e98b35a905
Luc Heuff <lucheuff@hotmail.com>
2023-05-17 12:17:02 +0200

    added half precision floats and lots of small changes

--- src/sentence_similarity.py
+++ src/sentence_similarity.py
@@ -8,27 +8,28 @@ def sentence_similarity(
         sentences: list[str],
         tokenize_method: str='words',
         lower: bool=False,
-        term_frequency: bool=True,  # ? perhaps remove option if it is always better to do or not do
-        scale_by_length: bool=False, # ? perhaps remove option if it is always better to do or not do
+        weight_matrix_min: float=0.1, # ? perhaps remove option if it is always better to do or not do
         ) -> pd.DataFrame:
 
     # Creating vocabulary to translate sentences into numbers
     vocab = Vocab(sentences, tokenize_method, lower)
     vocab_length = len(vocab)
-    # Numericalizing sentences
+
     num_sentences = _numericalize(sentences, vocab)
     max_sentence_length = max(len(sentence) for sentence in num_sentences)
-    # One-hot encoding 
-    one_hot_kwargs = dict(vocab_length=vocab_length, 
-                          max_sentence_length=max_sentence_length, 
-                          term_frequency=term_frequency, 
-                          scale_by_length=scale_by_length)
-    one_hot_encode = partial(_one_hot_sentence, **one_hot_kwargs)
-    one_hot_sentences = [one_hot_encode(sentence) for sentence in num_sentences]
-    
-    one_hot_tensor = np.stack(one_hot_sentences)
 
-    pass
+    one_hot_encode = partial(_one_hot_sentence, vocab_length=vocab_length, max_sentence_length=max_sentence_length)
+    one_hot_sentences = [one_hot_encode(sentence) for sentence in num_sentences]
+
+    one_hot_tensor = np.stack(one_hot_sentences)
+    weight_matrix = _weight_matrix(max_sentence_length, weight_matrix_min)
+
+    similarity = _einsum(one_hot_tensor, weight_matrix)
+
+    return _to_dataframe(sentences, similarity)
+
+def _half_precision(array: np.ndarray) -> np.ndarray:
+    return array.astype(dtype=np.float16)
 
 def _numericalize(sentences: list[str], vocab: Vocab) -> list[np.ndarray]:
     return [np.asarray(vocab.encode(sentence)) for sentence in sentences]
@@ -37,31 +38,25 @@ def _one_hot_sentence(
         sentence: np.ndarray, 
         vocab_length: int, 
         max_sentence_length: int,
-        term_frequency: bool,
-        scale_by_length: bool,
         ) -> np.ndarray:
-    # starting with a matrix with a row for each word in the vocab, and a column for each possible word in a sentence
+    # starting with a matrix of zeros with a row for each word in the vocab, and a column for each possible word in a sentence
     one_hot = np.zeros((vocab_length, max_sentence_length))
     # replacing indices given from the numericalised sentences with ones
     one_hot[(sentence, np.arange(len(sentence)))] = 1.
-    # Index 0 indicates that the token was not recognised by the vocabulary. These need to be ignored in the similarity check,
-    # so setting the first row to zeros:
+    # Index 0 indicates that the token was not recognised by the vocabulary. These need to be ignored in the similarity check, so setting the first row to zeros:
     one_hot[0] = np.zeros((1, max_sentence_length))
 
-    # when term frequency is applied, the value of a word appearing is divided by how often it appears in the sentence.
+    # Term frequency is applied to discount words that appear in the sentence more often. 
     # This avoids getting high similarity scores because the same word appears mulitple times in the sentence.
-    if term_frequency:
-        term_frequencies = one_hot.sum(axis=1) # counting how often each word appears by summing over the columns
-        inverse_term_frequencies = np.divide(1, term_frequencies, where=term_frequencies > 0) # avoiding divide by zero error with np.divide(where=...)
-        # multiply row by inverse term frequencies
-        one_hot = np.einsum("ij, i -> ij", one_hot, inverse_term_frequencies, optimize="optimal")
-    
-    if scale_by_length:
-        one_hot = one_hot * 1/len(sentence)
+    # Discounting with 1/sqrt(n), so my similarity score doesn't disappear too much simply through repeated words
+    term_frequencies = one_hot.sum(axis=1) # counting how often each word appears by summing over the columns
+    inverse_term_frequencies = np.divide(1, np.sqrt(term_frequencies), where=term_frequencies > 0) # avoiding divide by zero error with np.divide(where=...)
+    # multiply row by inverse term frequencies
+    one_hot = np.einsum("ij, i -> ij", one_hot, inverse_term_frequencies, optimize="optimal")
 
-    return one_hot
+    return _half_precision(one_hot)  # * using half precision floats to save some space
 
-def _weight_matrix(size: int, min: float=0.5) -> np.ndarray:
+def _weight_matrix(size: int, min: float) -> np.ndarray:
 
     if min >= 1.0:
         raise ValueError(f"You are trying to set a minimum value of {min}, which is higher or equal to 1. The weight matrix was not designed with this in mind")
@@ -72,6 +67,8 @@ def _weight_matrix(size: int, min: float=0.5) -> np.ndarray:
     weights = sum(np.eye(size, k=n) * s for n, s in zip(size_range, linear_space))
     weight_matrix = np.triu(weights) + np.triu(weights).T - np.eye(size)
     
+    return _half_precision(weight_matrix)
+
 def _einsum(tensor: np.ndarray, weight_matrix: np.ndarray) -> np.ndarray:
     # einsum to calculate the similarity scores for all sentences amongst each other
     similarity = np.einsum("mij, nik, jk -> mn", tensor, tensor, weight_matrix, optimize="optimal")
