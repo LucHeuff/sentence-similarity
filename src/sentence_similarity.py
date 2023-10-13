6a2bb9ce54d3d2944540d2c394f1c3fe70f823f4
Luc Heuff <lucheuff@hotmail.com>
2023-10-13 13:47:26 +0200

    fix: formatting and linting issues

--- src/sentence_similarity.py
+++ src/sentence_similarity.py
@@ -1,39 +1,56 @@
-import numpy as np
-import pandas as pd
 from functools import partial
 from typing import Protocol
 
-from src.translator import create_default_translator, tokenize_function, tokenize_on_spaces
+import numpy as np
+import pandas as pd
+
+from src.translator import (
+    create_default_translator,
+    tokenize_function,
+    tokenize_on_spaces,
+)
+
 
 class Translator(Protocol):
+    """Protocol class for translator methods"""
+
     def encode(self, sentence: str) -> list[int]:
+        """Translates a string sentence into a list of integers"""
         ...
 
     def __len__(self) -> int:
         """Returns the length of the vocabulary of the translator"""
         ...
 
+
 EINSUM_OPT = "optimal"
 
+
 def sentence_similarity(
-        sentences: list[str],
-        tokenizer: tokenize_function=tokenize_on_spaces,
-        translator: Translator | None = None,
-        weight_matrix_min: float=0.1, 
-        ) -> pd.DataFrame:
+    sentences: list[str],
+    tokenizer: tokenize_function = tokenize_on_spaces,
+    translator: Translator | None = None,
+    weight_matrix_min: float = 0.1,
+) -> pd.DataFrame:
     """Compares sentences in the form of strings through a tokenisation method.
 
     Args:
         sentences (list[str]): list of sentences to be compared to each other in the form of strings
-        tokenizer (tokenize_function, optional): function to perform tokenization. Also allows providing custom tokenization function. Defaults to tokenize_on_spaces.
-        translator (Translator | None, optional): Translator object that performs sentence encoding. Also allows providing a custom Translator object. Defaults to None.
-        weight_matrix_min (float, optional): The weight matrix discounts sentences that have the same words, but in different places. This value controls the weight of the value that is furthest out.
-                                             You may wish to raise the value if using short sentences or a small vocabulary. Defaults to 0.1.
+        tokenizer (tokenize_function, optional): function to perform tokenization.
+                 Also allows providing custom tokenization function. Defaults to tokenize_on_spaces.
+        translator (Translator | None, optional): Translator object that performs sentence encoding.
+                                 Also allows providing a custom Translator object. Defaults to None.
+        weight_matrix_min (float, optional): The weight matrix discounts sentences that have the
+            same words, but in different places. This value controls the weight of the value that
+            is furthest out. You may wish to raise the value if using short sentences or a small
+            vocabulary. Defaults to 0.1.
 
     Returns:
-        pd.DataFrame: containing each possible sentence pair and their corresponding similarity score. 
-                      A similarity score of 1 indicates the sentences are the same. A score below 1 means indicates a reduction in similarity.
-                      A score above 1 indicates that substrings in the sentence are repeated multiple times in the other sentence, which tends to happen more often when tokenizing characters.
+        pd.DataFrame: containing each possible sentence pair and the corresponding similarity score.
+                      A similarity score of 1 indicates the sentences are the same. A score below 1
+                      means indicates a reduction in similarity. A score above 1 indicates that
+                      substrings in the sentence are repeated multiple times in the other sentence,
+                      which tends to happen more often when tokenizing characters.
     """
     # Creating vocabulary to translate sentences into numbers
     if translator is None:
@@ -43,7 +60,11 @@ def sentence_similarity(
     num_sentences = _numericalize(sentences, translator)
     max_sentence_length = max(len(sentence) for sentence in num_sentences)
 
-    one_hot_encode = partial(_one_hot_sentence, vocab_length=vocab_length, max_sentence_length=max_sentence_length)
+    one_hot_encode = partial(
+        _one_hot_sentence,
+        vocab_length=vocab_length,
+        max_sentence_length=max_sentence_length,
+    )
     one_hot_sentences = [one_hot_encode(sentence) for sentence in num_sentences]
 
     one_hot_tensor = np.stack(one_hot_sentences)
@@ -53,14 +74,16 @@ def sentence_similarity(
 
     return _to_dataframe(sentences, similarity)
 
+
 def _numericalize(sentences: list[str], translator: Translator) -> list[np.ndarray]:
     return [np.asarray(translator.encode(sentence)) for sentence in sentences]
 
+
 def _one_hot_sentence(
-        sentence: np.ndarray, 
-        vocab_length: int, 
-        max_sentence_length: int,
-        ) -> np.ndarray:
+    sentence: np.ndarray,
+    vocab_length: int,
+    max_sentence_length: int,
+) -> np.ndarray:
     """Converts a numericalised sentence (e.g. [1, 2, 3]) into a matrix of one-hot encodings.
     Rows represent tokens in the vocabulary, columns represent the word order.
     Term frequency reduction is applied to handle cases where words are repeated in the sentence.
@@ -68,87 +91,121 @@ def _one_hot_sentence(
     Args:
         sentence (np.ndarray): sentence numericalised through the vocabulary
         vocab_length (int): total number of tokens in the vocabulary
-        max_sentence_length (int): length of the longest sentence in the comparison. 
-                                 This makes sure all matrices are of the same size, padded with zeros when shorter.
+        max_sentence_length (int): length of the longest sentence in the comparison.
+                                 This makes sure all matrices are of the same size,
+                                 padded with zeros when shorter.
 
     Returns:
-        np.ndarray: Matrix encoding of the sentence. Note that when term frequencies are applied, the matrix can take any value between 0. and 1.
+        np.ndarray: Matrix encoding of the sentence. Note that when term frequencies are applied,
+                    the matrix can take any value between 0. and 1.
     """
-    # starting with a matrix of zeros with a row for each word in the vocab, and a column for each possible word in a sentence
+    # starting with a matrix of zeros with a row for each word in the vocab,
+    # and a column for each possible word in a sentence
     one_hot = np.zeros((vocab_length, max_sentence_length))
     # replacing indices given from the numericalised sentences with ones
-    one_hot[(sentence, np.arange(len(sentence)))] = 1.
+    one_hot[(sentence, np.arange(len(sentence)))] = 1.0
 
-    # Term frequency is applied to discount words that appear in the sentence more often. 
-    # This avoids getting high similarity scores because the same word appears mulitple times in the sentence.
-    # Discounting with 1/sqrt(n), so my similarity score doesn't disappear too much simply through repeated words
-    term_frequencies = one_hot.sum(axis=1) # counting how often each word appears by summing over the columns
-    inverse_term_frequencies = np.divide(1, np.sqrt(term_frequencies), where=term_frequencies > 0) # avoiding divide by zero error with np.divide(where=...)
+    # Term frequency is applied to discount words that appear in the sentence more often.
+    # This avoids getting high similarity scores because the same word appears mulitple
+    # times in the sentence. Discounting with 1/sqrt(n), so my similarity score doesn't
+    # disappear too much simply through repeated words
+    term_frequencies = one_hot.sum(
+        axis=1
+    )  # counting how often each word appears by summing over the columns
+    inverse_term_frequencies = np.divide(
+        1, np.sqrt(term_frequencies), where=term_frequencies > 0
+    )  # avoiding divide by zero error with np.divide(where=...)
     # multiply row by inverse term frequencies
-    one_hot = np.einsum("ij, i -> ij", one_hot, inverse_term_frequencies, optimize=EINSUM_OPT)
-    one_hot = np.nan_to_num(one_hot) # making sure there are no nans in my array which seems to happen in testing
+    one_hot = np.einsum(
+        "ij, i -> ij", one_hot, inverse_term_frequencies, optimize=EINSUM_OPT
+    )
+    one_hot = np.nan_to_num(
+        one_hot
+    )  # making sure there are no nans in my array which seems to happen in testing
 
     return one_hot
 
-def _weight_matrix(size: int, min: float) -> np.ndarray:
-    """Generates a weight matrix to discount cases where words do appear in a pair of sentences, but in different positions in the sentence.
-    This matrix has 1.'s on the diagonal, and reduce out the the provided minimum value to the top-right and bottom-left corners of the matrix.
+
+def _weight_matrix(size: int, minimum: float) -> np.ndarray:
+    """Generates a weight matrix to discount cases where words do appear in a pair of sentences,
+    but in different positions in the sentence. This matrix has 1.'s on the diagonal, and reduce
+    out the the provided minimum value to the top-right and bottom-left corners of the matrix.
 
     Args:
         size (int): Determines the shape of the square matrix (size, size)
-        min (float): Determines the minimum value to enter into the weight matrix. Should be between 0. and 1. Setting this to 1 basically disables weight scaling.
+        minimum (float): Determines the minimum value to enter into the weight matrix.
+                         Should be between 0. and 1. Setting this to 1 disables weight scaling.
 
     Raises:
-        ValueError: When [min] is not between 0. and 1.
+        ValueError: When [minimum] is not between 0. and 1.
 
     Returns:
-        np.ndarray: weight matrix of shape (size, size) with 1. on diagonal and lowering to [min] on towards the edges.
+        np.ndarray: weight matrix of shape (size, size) with 1. on diagonal
+                    and lowering to [minimum] on towards the edges.
     """
 
-    if not 0 <= min <= 1:
-        raise ValueError(f"You are trying to set a minimum value for the weight matrix of {min}, which is outside the range [0., 1.] The weight matrix was not designed with this in mind")
+    if not 0 <= minimum <= 1:
+        raise ValueError(
+            f"""You are trying to set a minimum value for the weight matrix of {minimum},
+            which is outside the range [0., 1.].
+            The weight matrix was not designed with this in mind"""
+        )
 
     size_range = np.arange(size)
-    linear_space = np.linspace(1, min, num=size)
+    linear_space = np.linspace(1, minimum, num=size)
 
     weights = sum(np.eye(size, k=n) * s for n, s in zip(size_range, linear_space))
     weight_matrix = np.triu(weights) + np.triu(weights).T - np.eye(size)
-    
+
     return weight_matrix
 
+
 def _einsum(tensor: np.ndarray, weight_matrix: np.ndarray) -> np.ndarray:
-    """Calculates similarity among sentences by performing tensor inner product, scaling by the weight matrix, and scaling the result over the diagonal.
+    """Calculates similarity among sentences by performing tensor inner product,
+    scaling by the weight matrix, and scaling the result over the diagonal.
     This is implemented using np.einsum to vastly optimise the calculation time
 
     Args:
-        tensor (np.ndarray): encoded sentences in tensor form, of shape (sentences, vocabulary length, max sentence length)
+        tensor (np.ndarray): encoded sentences in tensor form,
+                             of shape (sentences, vocabulary length, max sentence length)
         weight_matrix (np.ndarray): weight matrix of shape (sentences, sentences)
 
     Returns:
         np.ndarray: similarity scores of shape (sentences, sentences)
     """
     # einsum to calculate the similarity scores for all sentences amongst each other
-    similarity = np.einsum("mij, nik, jk -> mn", tensor, tensor, weight_matrix, optimize=EINSUM_OPT)
+    similarity = np.einsum(
+        "mij, nik, jk -> mn", tensor, tensor, weight_matrix, optimize=EINSUM_OPT
+    )
 
-    # scaling down columnwise by diagonal, this should result in scoring in a column being relative to the sentence itself
-    similarity = np.einsum("ij, j -> ij", similarity, 1 / np.diag(similarity), optimize=EINSUM_OPT)
+    # scaling down columnwise by diagonal, this should result in
+    # scoring in a column being relative to the sentence itself
+    similarity = np.einsum(
+        "ij, j -> ij", similarity, 1 / np.diag(similarity), optimize=EINSUM_OPT
+    )
 
     return similarity
 
+
 def _to_dataframe(sentences: list[str], similarity: np.ndarray) -> pd.DataFrame:
-    """Constructs a pandas.DataFrame containing the combination of each pair of sentences with their similarity scores.
+    """Constructs a pandas.DataFrame containing the combination of
+    each pair of sentences with their similarity scores.
 
     Args:
         sentences (list[str]): list of sentences that were compared to each other using _einsum()
         similarity (np.ndarray): output of _einsum()
 
     Returns:
-        pd.DataFrame: dataframe consisting of sentence pair columns named 'sentence' and 'other_sentence' with their similarity score in 'similarity'
+        pd.DataFrame: dataframe consisting of sentence pair columns named 'sentence'
+                    and 'other_sentence' with their similarity score in 'similarity'
     """
 
-    new_names = {'level_0': 'sentence', 'level_1': 'other_sentence', 0: 'similarity'}
+    new_names = {"level_0": "sentence", "level_1": "other_sentence", 0: "similarity"}
     df = (
         pd.DataFrame(similarity, index=sentences, columns=sentences)
-        .stack().reset_index().rename(columns=new_names)
-          )
+        .stack()
+        .reset_index()
+        .rename(columns=new_names)
+    )
     return df
+
--- tests/test_sentence_similarity.py
+++ tests/test_sentence_similarity.py
@@ -1,70 +1,97 @@
 import itertools
 import string
-import numpy as np
+
 import hypothesis.strategies as st
-from hypothesis import given, assume
-from hypothesis.strategies import composite
+import numpy as np
+from hypothesis import assume, given
 from hypothesis.extra.numpy import arrays
+from hypothesis.strategies import composite
 from pytest import raises
 
-from src.translator import create_default_translator, tokenize_function, tokenize_on_spaces, tokenize_characters
+from src.translator import (
+    create_default_translator,
+    tokenize_characters,
+    tokenize_function,
+    tokenize_on_spaces,
+)
 
 tokenizers = [tokenize_on_spaces, tokenize_characters]
-alphabet = string.ascii_letters + string.digits + string.punctuation.replace("\\", "")
+ALPHABET = string.ascii_letters + string.digits + string.punctuation.replace("\\", "")
 
 WORD_LENGTH = 20
 VOCAB_LENGTH = 15
 SENTENCE_LENGTH = 10
 
 # Nomenclature:
-# - a generator is a sampler that is meant to be reused in multiple places, but not directly as a strategy for a test.
-# - a strategy is a sampler that is bespoke to a function test. Strategies can output multiple objects.
+# - a generator is a sampler that is meant to be reused in multiple places,
+#   but not directly as a strategy for a test.
+# - a strategy is a sampler that is bespoke to a function test.
+#   Strategies can output multiple objects.
 
 # ---- standard generators -----
+
 
 @composite
 def word_generator(draw) -> str:
     """Generates a single 'word' that is not allowed to be only empty spaces"""
-    word = draw(st.text(alphabet, min_size=1, max_size=WORD_LENGTH))
-    # assume(word != " ") # words are not allowed to be spaces 
+    word = draw(st.text(ALPHABET, min_size=1, max_size=WORD_LENGTH))
+    # assume(word != " ") # words are not allowed to be spaces
     return word
+
 
 @composite
 def sentence_generator(draw) -> str:
     """Generates a sentence of one or more 'words"""
-    words = draw(st.lists(word_generator(), min_size=1)) 
+    words = draw(st.lists(word_generator(), min_size=1))
     sentence = " ".join(words)
     # assume(sentence != "") # not allowed to be empty, I'm just assuming sentences are never empty
     return sentence
 
+
 # ---- Testing numericalization ----
 from src.sentence_similarity import _numericalize
 
+
 @composite
-def sentences_generator(draw, min_length: int=1, max_length: int=SENTENCE_LENGTH) -> list[str]:
+def sentences_generator(
+    draw, min_length: int = 1, max_length: int = SENTENCE_LENGTH
+) -> list[str]:
     """Generate a list of unique sentences"""
-    return draw(st.lists(sentence_generator(), min_size=min_length, max_size=max_length, unique=True))
+    return draw(
+        st.lists(
+            sentence_generator(), min_size=min_length, max_size=max_length, unique=True
+        )
+    )
+
 
 @given(sentences=sentences_generator(), method=st.sampled_from(tokenizers))
 def test_numericalize(sentences, method):
     """
-     - Test whether sentences are converted to numpy.ndarrays
-     - Test if the same number of sentences are numericalised as are entered in the numericalisation
+    - Test whether sentences are converted to numpy.ndarrays
+    - Test if the same number of sentences are numericalised as are entered in the numericalisation
     """
     translator = create_default_translator(sentences, method)
     numericalized = _numericalize(sentences, translator)
     assert all(isinstance(sentence, np.ndarray) for sentence in numericalized)
     assert len(numericalized) == len(sentences)
 
+
 # ---- Testing one-hot encoding ----
 from src.sentence_similarity import _one_hot_sentence
 
+
 @composite
-def numbered_sentence_generator(draw, min_length: int=1, max_length: int=SENTENCE_LENGTH, vocab_length: int=VOCAB_LENGTH) -> np.ndarray:
+def numbered_sentence_generator(
+    draw,
+    min_length: int = 1,
+    max_length: int = SENTENCE_LENGTH,
+    vocab_length: int = VOCAB_LENGTH,
+) -> np.ndarray:
     """Generates a 'sentence' as a list of numbers as a np.ndarray"""
     nums = st.integers(min_value=0, max_value=vocab_length)
     length = draw(st.integers(min_value=min_length, max_value=max_length))
     return draw(arrays(dtype=np.int64, shape=length, elements=nums))
+
 
 @given(num_sentence=numbered_sentence_generator())
 def test_one_hot_sentence(num_sentence: np.ndarray):
@@ -72,7 +99,9 @@ def test_one_hot_sentence(num_sentence: np.ndarray):
     - Test if the one-hot-encoded matrix has dimensions of (vocab_length, max_sentence_length)
     - Test if the correct index in each column is one-hot-encoded
     """
-    vocab_length = num_sentence.max() + 1  # making sure that if the sentence is [0] this is interpreted as a length of 1
+    vocab_length = (
+        num_sentence.max() + 1
+    )  # making sure that if the sentence is [0] this is interpreted as a length of 1
     max_sentence_length = num_sentence.size
 
     encoded = _one_hot_sentence(num_sentence, vocab_length, max_sentence_length)
@@ -80,12 +109,14 @@ def test_one_hot_sentence(num_sentence: np.ndarray):
     assert encoded.shape == (vocab_length, max_sentence_length)
     assert np.array_equal(num_sentence, encoded.argmax(axis=0))
 
+
 # ---- Testing weight matrix -----
 from src.sentence_similarity import _weight_matrix
 
+
 @given(
     size=st.integers(min_value=2, max_value=10),
-    min=st.floats(min_value=0, max_value=0.99) 
+    min=st.floats(min_value=0, max_value=0.99),
 )
 def test_weight_matrix(size: int, min: float):
     """
@@ -98,14 +129,17 @@ def test_weight_matrix(size: int, min: float):
     weight_matrix = _weight_matrix(size, min)
 
     assert weight_matrix.shape == (size, size)
-    assert weight_matrix.max() == 1.
+    assert weight_matrix.max() == 1.0
     assert weight_matrix.min() == min
     assert np.array_equal(weight_matrix.T, weight_matrix)
     assert all(np.diag(weight_matrix))
 
+
 @given(
     size=st.integers(min_value=2, max_value=10),
-    min=st.floats().filter(lambda x: not 0. <= x <= 1.)  # generating floats outside of the range [0, 1]
+    min=st.floats().filter(
+        lambda x: not 0.0 <= x <= 1.0
+    ),  # generating floats outside of the range [0, 1]
 )
 def test_weight_matrix_exception(size: int, min: float):
     """
@@ -118,21 +152,39 @@ def test_weight_matrix_exception(size: int, min: float):
 # ---- Testing einsum ----
 from src.sentence_similarity import _einsum
 
+
 @composite
-def numbered_sentences_generator(draw, sentence_length: int=SENTENCE_LENGTH, vocab_length: int=VOCAB_LENGTH) -> list[np.ndarray]:
+def numbered_sentences_generator(
+    draw, sentence_length: int = SENTENCE_LENGTH, vocab_length: int = VOCAB_LENGTH
+) -> list[np.ndarray]:
     """Generate a list of 'numbered_sentence's"""
     size = sentence_length
-    return draw(st.lists(numbered_sentence_generator(min_length=size, max_length=size, vocab_length=vocab_length), min_size=2))
+    return draw(
+        st.lists(
+            numbered_sentence_generator(
+                min_length=size, max_length=size, vocab_length=vocab_length
+            ),
+            min_size=2,
+        )
+    )
+
 
 @composite
 def einsum_strategy(draw) -> tuple[list[np.ndarray], int, int]:
-    """Generates a list of numbered sentences, a vocab length and a sentence length for testing einsum calculations    
-    """
+    """Generates a list of numbered sentences,
+    a vocab length and a sentence length for testing einsum calculations"""
     vocab_length = draw(st.integers(min_value=1, max_value=30))
     sentence_length = draw(st.integers(min_value=2, max_value=30))
-    sentences = draw(numbered_sentences_generator(sentence_length=sentence_length, vocab_length=vocab_length))
-    assume(sentences != []) # Assuming sentences are not empty because they are annoying
+    sentences = draw(
+        numbered_sentences_generator(
+            sentence_length=sentence_length, vocab_length=vocab_length
+        )
+    )
+    assume(
+        sentences != []
+    )  # Assuming sentences are not empty because they are annoying
     return sentences, vocab_length + 1, sentence_length
+
 
 # somewhat more of an integeration test but generating the right tensors is really annoying
 @given(einsum_strategy())
@@ -144,9 +196,12 @@ def test_einsum(data: tuple[list[np.ndarray], int, int]):
     """
     sentences, vocab_length, sentence_length = data
 
-    one_hot_sentences = [_one_hot_sentence(sentence, vocab_length, sentence_length) for sentence in sentences]
+    one_hot_sentences = [
+        _one_hot_sentence(sentence, vocab_length, sentence_length)
+        for sentence in sentences
+    ]
     tensor = np.stack(one_hot_sentences)
-    weight_matrix = _weight_matrix(size=sentence_length, min=0.2)
+    weight_matrix = _weight_matrix(size=sentence_length, minimum=0.2)
 
     einsum = _einsum(tensor, weight_matrix)
 
@@ -159,6 +214,7 @@ def test_einsum(data: tuple[list[np.ndarray], int, int]):
 # ---- Testing to_dataframe ----
 from src.sentence_similarity import _to_dataframe
 
+
 @composite
 def dataframe_strategy(draw) -> tuple[list[str], np.ndarray]:
     """Generates fake einsum output that can be converted to a dataframe.
@@ -166,12 +222,16 @@ def dataframe_strategy(draw) -> tuple[list[str], np.ndarray]:
     """
     sents = draw(sentences_generator(min_length=2))
     # Generating a fake einsum matrix
-    data = draw(arrays(
-        dtype=np.float64,
-        shape=(len(sents), len(sents)),
-        elements=st.floats(min_value=0, allow_infinity=False),
-        unique=True))
+    data = draw(
+        arrays(
+            dtype=np.float64,
+            shape=(len(sents), len(sents)),
+            elements=st.floats(min_value=0, allow_infinity=False),
+            unique=True,
+        )
+    )
     return sents, data
+
 
 @given(dataframe_strategy())
 def test_to_dataframe(data: tuple[list[str], np.ndarray]):
@@ -185,10 +245,10 @@ def test_to_dataframe(data: tuple[list[str], np.ndarray]):
 
     df = _to_dataframe(sentences, similarity)
 
-    assert df.shape == (len(sentences)**2, 3)
-    assert np.array_equal(df.columns, ['sentence', 'other_sentence', 'similarity'])
-    assert np.array_equal(df.sentence.unique(), sentences) 
-    assert np.array_equal(df.other_sentence.unique(), sentences) 
+    assert df.shape == (len(sentences) ** 2, 3)
+    assert np.array_equal(df.columns, ["sentence", "other_sentence", "similarity"])
+    assert np.array_equal(df.sentence.unique(), sentences)
+    assert np.array_equal(df.other_sentence.unique(), sentences)
 
     # checking whether values have ended up in the right place
     indices = list(range(len(sentences)))
@@ -202,21 +262,29 @@ def test_to_dataframe(data: tuple[list[str], np.ndarray]):
 # ---- integration test of sentence_similarity ----
 from src.sentence_similarity import sentence_similarity
 
+
 @given(
     sentences=sentences_generator(),
     tokenizer=st.sampled_from(tokenizers),
-    weight_matrix_min=st.floats(min_value=0, max_value=0.999)
+    weight_matrix_min=st.floats(min_value=0, max_value=0.999),
 )
-def test_sentence_similarity(sentences: list[str], tokenizer: tokenize_function, weight_matrix_min: float):
+def test_sentence_similarity(
+    sentences: list[str], tokenizer: tokenize_function, weight_matrix_min: float
+):
     """Integration test of sentence similarity:
     # - Test whether similarity data frame has the correct shape
     # - Test whether each sentence is in both [sentence] and [other_sentence] columns
     # - Test whether the provided sentences all show up in the [sentence] column
     # - Test whether comparing each sentence with itself results in a similarity score of 1
     """
-    similarity = sentence_similarity(sentences, tokenizer, weight_matrix_min=weight_matrix_min)
+    similarity = sentence_similarity(
+        sentences, tokenizer, weight_matrix_min=weight_matrix_min
+    )
 
-    assert similarity.shape == (len(sentences)**2, 3)
-    assert np.array_equal(similarity.sentence.unique(), similarity.other_sentence.unique())
+    assert similarity.shape == (len(sentences) ** 2, 3)
+    assert np.array_equal(
+        similarity.sentence.unique(), similarity.other_sentence.unique()
+    )
     assert np.array_equal(similarity.sentence.unique(), sentences)
-    assert similarity.query('sentence == other_sentence').similarity.all()
+    assert similarity.query("sentence == other_sentence").similarity.all()
+
