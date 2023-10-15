553ee0589e54552f02ad43ebd7b2cace99e4c4fc
Luc Heuff <lucheuff@hotmail.com>
2023-10-15 18:12:22 +0200

    fix: problems with package structure

---
+++ sentence_similarity/__init__.py
@@ -0,0 +1,15 @@
+from sentence_similarity.sentence_similarity import (
+    TokenizeFunction,
+    Translator,
+    sentence_similarity,
+)
+from sentence_similarity.translator import (
+    create_string_distance_vocab,
+    create_synonym_vocab,
+    create_translator,
+    create_vocab,
+    tokenize_characters,
+    tokenize_on_spaces,
+    tokenize_words,
+)
+
