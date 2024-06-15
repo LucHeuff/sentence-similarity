1d2b86f987e96fb9cff7503a6c0ccd7d34d1622f
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-06-15 11:36:32 +0200

    feature: added benchmarking file

---
+++ benchmark.py
@@ -0,0 +1,41 @@
+import cProfile
+import warnings
+from pstats import Stats
+from time import perf_counter
+
+import numpy as np
+
+from sentence_similarity import sentence_similarity
+
+warnings.simplefilter("ignore", DeprecationWarning)
+
+import pandas as pd
+
+bench = pd.read_csv("bench.csv")
+
+# Benchmarking
+
+N = 10
+
+times = []
+
+for _ in range(N):
+    start = perf_counter()
+    sentence_similarity(bench.omschrijving.tolist())
+    end = perf_counter()
+    times.append(end - start)
+
+result = np.asarray(times)
+
+print(" ---- BENCHMARK ----")
+print(f"Average time: {result.mean():.3f} ± {result.std():.3f} s\n\n")
+
+print(" ---- PROFILE ----")
+
+# Profiling
+PROFILE = True
+
+if PROFILE:
+    with cProfile.Profile() as profiler:
+        sentence_similarity(bench.omschrijving.tolist())
+        Stats(profiler).strip_dirs().sort_stats("cumulative").print_stats(20)
