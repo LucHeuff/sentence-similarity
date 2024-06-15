0712387896a709145b2792e9f535280f46eca0ab
Luc Heuff <10941592+LucHeuff@users.noreply.github.com>
2024-06-15 13:58:18 +0200

    experiment with sparse matrices instead

--- sentence_similarity/sentence_similarity.py
+++ sentence_similarity/sentence_similarity.py
@@ -1,9 +1,11 @@
 """Contains sentence similarity main function and algorithm."""
+from collections import Counter
 from functools import partial
 from typing import Protocol
 
 import numpy as np
 import pandas as pd
+import sparse
 
 from sentence_similarity.translator import (
     TokenizeFunction,
@@ -90,7 +92,7 @@ def sentence_similarity(
     )
     one_hot_sentences = [one_hot_encode(sentence) for sentence in num_sentences]
 
-    one_hot_tensor = np.stack(one_hot_sentences)
+    one_hot_tensor = sparse.stack(one_hot_sentences)
     similarity = _einsum(one_hot_tensor, weight_matrix)
 
     return _to_dataframe(sentences, similarity, filter_identity=filter_identity)
@@ -106,7 +108,7 @@ def _one_hot_sentence(
     sentence: np.ndarray,
     vocab_length: int,
     max_sentence_length: int,
-) -> np.ndarray:
+) -> sparse.COO:
     """Convert a numericalised sentence (e.g. [1, 2, 3]) into a matrix of one-hot encodings.
 
     Rows represent tokens in the vocabulary, columns represent the word order.
@@ -126,30 +128,45 @@ def _one_hot_sentence(
                     the matrix can take any value between 0. and 1.
 
     """
-    # starting with a matrix of zeros with a row for each word in the vocab,
-    # and a column for each possible word in a sentence
-    one_hot = np.zeros((vocab_length, max_sentence_length))
-    # replacing indices given from the numericalised sentences with ones
-    one_hot[(sentence, np.arange(len(sentence)))] = 1.0
+    # # starting with a matrix of zeros with a row for each word in the vocab,
+    # # and a column for each possible word in a sentence
+    # one_hot = np.zeros((vocab_length, max_sentence_length))
+    # # replacing indices given from the numericalised sentences with ones
+    # one_hot[(sentence, np.arange(len(sentence)))] = 1.0
+    #
+    # # Term frequency is applied to discount words that appear in the sentence more often.
+    # # This avoids getting high similarity scores because the same word appears multiple
+    # # times in the sentence. Discounting with 1/sqrt(n), so my similarity score doesn't
+    # # disappear too much simply through repeated words
+    #
+    # # counting how often each word appears by summing over the columns
+    # term_frequencies = one_hot.sum(axis=1)
+    #
+    # # avoiding divide by zero error with np.divide(where=...)
+    # inverse_term_frequencies = np.divide(
+    #     1, np.sqrt(term_frequencies), where=term_frequencies > 0
+    # )
+    # # multiply row by inverse term frequencies
+    # one_hot = np.einsum(
+    #     "ij, i -> ij", one_hot, inverse_term_frequencies, optimize=EINSUM_OPT
+    # )
+    # # making sure there are no nans in my array which seems to happen in testing
 
-    # Term frequency is applied to discount words that appear in the sentence more often.
-    # This avoids getting high similarity scores because the same word appears mulitple
-    # times in the sentence. Discounting with 1/sqrt(n), so my similarity score doesn't
-    # disappear too much simply through repeated words
+    # sparse variant
 
-    # counting how often each word appears by summing over the columns
-    term_frequencies = one_hot.sum(axis=1)
+    # Since words can occur multiple times in a sentence, repeated words need to be discounted
+    # so comparing a sentence with itself doesn't increase above 1 due to repeated words.
+    counter = Counter(sentence)
+    data = [1 / np.sqrt(counter[word]) for word in sentence]
 
-    # avoiding divide by zero error with np.divide(where=...)
-    inverse_term_frequencies = np.divide(
-        1, np.sqrt(term_frequencies), where=term_frequencies > 0
+    # index is the location of the word in the sentence, needed to build the sparse matrix
+    index = list(range(len(sentence)))
+
+    return sparse.COO(
+        [sentence, index],
+        data,
+        shape=(vocab_length, max_sentence_length),
     )
-    # multiply row by inverse term frequencies
-    one_hot = np.einsum(
-        "ij, i -> ij", one_hot, inverse_term_frequencies, optimize=EINSUM_OPT
-    )
-    # making sure there are no nans in my array which seems to happen in testing
-    return np.nan_to_num(one_hot)
 
 
 def _weight_matrix(
@@ -204,7 +221,9 @@ def _weight_matrix(
     return weight_matrix
 
 
-def _einsum(tensor: np.ndarray, weight_matrix: np.ndarray) -> np.ndarray:
+def _einsum(
+    tensor: sparse.COO | sparse.GCXS, weight_matrix: np.ndarray
+) -> np.ndarray:
     """Calculate similarity among sentences.
 
     Similarity is calculated by performing tensor inner product,
@@ -223,14 +242,28 @@ def _einsum(tensor: np.ndarray, weight_matrix: np.ndarray) -> np.ndarray:
 
     """
     # einsum to calculate the similarity scores for all sentences amongst each other
-    similarity = np.einsum(
-        "mij, nik, jk -> mn", tensor, tensor, weight_matrix, optimize=EINSUM_OPT
-    )
 
+    # numpy version
+    # similarity = np.einsum(
+    #     "mij, nik, jk -> mn", tensor, tensor, weight_matrix, optimize=EINSUM_OPT
+    # )
     # scaling down columnwise by diagonal, this should result in
     # scoring in a column being relative to the sentence itself
-    return np.einsum(
-        "ij, j -> ij", similarity, 1 / np.diag(similarity), optimize=EINSUM_OPT
+    # return np.einsum(
+    #     "ij, j -> ij", similarity, 1 / np.diag(similarity), optimize=EINSUM_OPT
+    # )
+
+    # sparse version
+    # weight_matrix = sparse.COO.from_numpy(weight_matrix)
+
+    similarity = sparse.einsum(
+        "mij, nik, jk -> mn", tensor, tensor, weight_matrix, optimize="optimal"
+    )
+    return similarity.todense()
+    # scaling down columnwise by diagonal, this should result in
+    # scoring in a column being relative to the sentence itself
+    return sparse.einsum(
+        "ij, j -> ij", similarity, 1 / sparse.diagonal(similarity)
     )
 
 
